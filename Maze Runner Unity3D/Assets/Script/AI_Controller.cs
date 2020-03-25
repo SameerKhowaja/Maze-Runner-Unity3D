3ef3b62cb74e5c1834981d06e32adea21cc8731a
Sameer Khowaja <sameerkhowaja12@gmail.com>
2020-03-25 17:17:30 +0500

    Gun Added with Enemy Destruction Script

---
+++ Maze Runner Unity3D/Assets/Script/AI_Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI_Controller : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform PlayerPos;
+
+    private void Start()
+    {
+        PlayerPos = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        agent.speed = Random.Range(0.4f, 1f);
+        agent.SetDestination(PlayerPos.position);
+    }
+}
---
+++ Maze Runner Unity3D/Assets/Script/gunScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunScript : MonoBehaviour
+{
+    public float damage;
+    public float range;
+
+    public Camera Gun_Cam;
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hitInfo;
+        if(Physics.Raycast(Gun_Cam.transform.position, Gun_Cam.transform.forward, out hitInfo, range))
+        {
+            Debug.Log(hitInfo.transform.name);
+
+            Bot_Health target = hitInfo.transform.GetComponent<Bot_Health>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
